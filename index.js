391192c9ff5dbd846d905662efadc856872d2122
Jessica Feng <jessicafeng14@yahoo.com>
2020-08-16 14:46:20 -0700

    added functions to create readme format

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// required packages and generateMarkdown.js
+const fs = require("fs");
+const inquirer = require("inquirer");
+const utils = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
     {
@@ -17,14 +22,14 @@ const questions = [
     },
     {
     type: "input",
-    message: "Please write a short description of your project"
+    message: "Please write a short description of your project",
     name: "description"    
     },
     {
     type: "list",
-    message: "What kind of license should your project have?"    
+    message: "What kind of license should your project have?",    
     name: "license",
-    choices: ["MIT","MPL","GPL","Apache","Boost"]
+    choices: ["MIT","APACHE 2.0","GPL 3.0","BSD 3","None"]
     },
     {
     type: "input",
@@ -39,17 +44,28 @@ const questions = [
     {
     type: "input",
     message: "What does the user need to know about using the repo?",
+    name: "usage"
+    },
+    {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
     name: "contributing"    
     }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    const inputData = utils(data);
+    fs.appendFile(fileName,inputData,(err)=>{if(err){console.log(err)}});
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then((response) => {
+        writeToFile("README.md",response);
+    })
+    .catch((err)=>{console.log(err)});
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,75 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+let selectedLicense = [];
 
-`;
+function generateMarkdown(data) {
+  switch(data.license){
+    case "MIT":
+    selectedLicense[0] = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    selectedLicense[1] = "(https://opensource.org/licenses/MIT)";
+    break;
+    case "APACHE 2.0":
+    selectedLicense[0] = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    selectedLicense[1] = "(https://opensource.org/licenses/Apache-2.0)";
+    break;
+    case "GPL 3.0":
+    selectedLicense[0] = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+    selectedLicense[1] = "(https://www.gnu.org/licenses/gpl-3.0)";
+    break;
+    case "BSD 3":
+    selectedLicense[0] = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+    selectedLicense[1] = "(https://opensource.org/licenses/BSD-3-Clause)";
+    break;
+    case "None":
+    selectedLicense[0] = "";
+    selectedLicense[1] = "None";  
+    break;
+  }
+  return `# ${data.projectname}
+
+  ${selectedLicense[0]}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation} is used to install dependencies.
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${selectedLicense[0]}
+  ${selectedLicense[1]}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests} is used to run tests.
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}.
+  You can find more of my work at [${data.username}](https://github.com/${data.projectname}).`;
 }
 
+//Export function for us with index.js.
 module.exports = generateMarkdown;
